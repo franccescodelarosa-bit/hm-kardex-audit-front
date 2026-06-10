363904838d7b73a514c82b92479dfd9f19730031
Frank De La Rosa <frankdelarosacampos@gmail.com>
2026-06-10 16:19:45 -0500

    add login

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">
-          HMSoft
-        </h1>
-
-        <Link
-          href="/login"
-          className="inline-block mt-4 px-5 py-3 rounded-lg bg-black text-white"
-        >
-          Ir al Login
-        </Link>
-      </div>
-    </div>
-  );
+  redirect("/login");
 }
