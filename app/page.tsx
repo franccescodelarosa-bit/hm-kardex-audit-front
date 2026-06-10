f7e5890e7a3f65e4c65f3f161e5581b69a02faa7
Frank De La Rosa <frankdelarosacampos@gmail.com>
2026-06-10 16:11:14 -0500

    test: remove login

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  redirect("/login");
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">
+          HMSoft
+        </h1>
+
+        <Link
+          href="/login"
+          className="inline-block mt-4 px-5 py-3 rounded-lg bg-black text-white"
+        >
+          Ir al Login
+        </Link>
+      </div>
+    </div>
+  );
 }
